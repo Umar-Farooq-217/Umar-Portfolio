9f805d7b4e07076aac6370a97e4dc9efd9ae57c1
Umar Farooq <uf29664@gmail.com>
2024-10-25 14:28:49 +0500

    portfo

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <div className="bg"></div>
+      <div className="bg-[url('/bf.webp')] bg-center bg-no-repeat bg-cover w-full h-full" ></div>
       <div className="">
         <Image src='/Unititled.svg' height={50} width={50} alt="logo"/>
       </div>
