2b17fb6fe3b07bbaf192faae2784e57aa3f3f8c2
Umar Farooq <uf29664@gmail.com>
2024-10-25 14:24:52 +0500

    portfolio

--- app/page.js
+++ app/page.js
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=""></div>
+    <div className="">
+      <div className="bg"></div>
+      <div className="">
+        <Image src='/Unititled.svg' height={50} width={50} alt="logo"/>
+      </div>
+    </div>
   );
 }
